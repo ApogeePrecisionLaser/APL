d9c1d646e78151b8513c8192ce1f9a181f4ce7d7
Vikrant <DELL@DESKTOP-9NBS7DC.Dlink>
2021-08-18 12:21:27 +0530

    Class updates

---
+++ src/java/com/organization/tableClasses/GenerateSpreadSheet.java
@@ -0,0 +1,327 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.organization.tableClasses;
+
+/**
+ *
+ * @author DELL
+ */
+public class GenerateSpreadSheet {
+
+    /**
+     * @return the key_person_id
+     */
+    public int getKey_person_id() {
+        return key_person_id;
+    }
+
+    /**
+     * @param key_person_id the key_person_id to set
+     */
+    public void setKey_person_id(int key_person_id) {
+        this.key_person_id = key_person_id;
+    }
+
+    /**
+     * @return the emp_code
+     */
+    public int getEmp_code() {
+        return emp_code;
+    }
+
+    /**
+     * @param emp_code the emp_code to set
+     */
+    public void setEmp_code(int emp_code) {
+        this.emp_code = emp_code;
+    }
+
+    /**
+     * @return the designation_code
+     */
+    public int getDesignation_code() {
+        return designation_code;
+    }
+
+    /**
+     * @param designation_code the designation_code to set
+     */
+    public void setDesignation_code(int designation_code) {
+        this.designation_code = designation_code;
+    }
+
+    /**
+     * @return the key_person_name
+     */
+    public String getKey_person_name() {
+        return key_person_name;
+    }
+
+    /**
+     * @param key_person_name the key_person_name to set
+     */
+    public void setKey_person_name(String key_person_name) {
+        this.key_person_name = key_person_name;
+    }
+
+    /**
+     * @return the kp_address_line1
+     */
+    public String getKp_address_line1() {
+        return kp_address_line1;
+    }
+
+    /**
+     * @param kp_address_line1 the kp_address_line1 to set
+     */
+    public void setKp_address_line1(String kp_address_line1) {
+        this.kp_address_line1 = kp_address_line1;
+    }
+
+    /**
+     * @return the kp_address_line2
+     */
+    public String getKp_address_line2() {
+        return kp_address_line2;
+    }
+
+    /**
+     * @param kp_address_line2 the kp_address_line2 to set
+     */
+    public void setKp_address_line2(String kp_address_line2) {
+        this.kp_address_line2 = kp_address_line2;
+    }
+
+    /**
+     * @return the kp_address_line3
+     */
+    public String getKp_address_line3() {
+        return kp_address_line3;
+    }
+
+    /**
+     * @param kp_address_line3 the kp_address_line3 to set
+     */
+    public void setKp_address_line3(String kp_address_line3) {
+        this.kp_address_line3 = kp_address_line3;
+    }
+
+    /**
+     * @return the kp_mobile_no1
+     */
+    public String getKp_mobile_no1() {
+        return kp_mobile_no1;
+    }
+
+    /**
+     * @param kp_mobile_no1 the kp_mobile_no1 to set
+     */
+    public void setKp_mobile_no1(String kp_mobile_no1) {
+        this.kp_mobile_no1 = kp_mobile_no1;
+    }
+
+    /**
+     * @return the kp_email_id1
+     */
+    public String getKp_email_id1() {
+        return kp_email_id1;
+    }
+
+    /**
+     * @param kp_email_id1 the kp_email_id1 to set
+     */
+    public void setKp_email_id1(String kp_email_id1) {
+        this.kp_email_id1 = kp_email_id1;
+    }
+
+    /**
+     * @return the kp_father_name
+     */
+    public String getKp_father_name() {
+        return kp_father_name;
+    }
+
+    /**
+     * @param kp_father_name the kp_father_name to set
+     */
+    public void setKp_father_name(String kp_father_name) {
+        this.kp_father_name = kp_father_name;
+    }
+
+    /**
+     * @return the kp_date_of_birth
+     */
+    public String getKp_date_of_birth() {
+        return kp_date_of_birth;
+    }
+
+    /**
+     * @param kp_date_of_birth the kp_date_of_birth to set
+     */
+    public void setKp_date_of_birth(String kp_date_of_birth) {
+        this.kp_date_of_birth = kp_date_of_birth;
+    }
+
+    /**
+     * @return the emergency_contact_name
+     */
+    public String getEmergency_contact_name() {
+        return emergency_contact_name;
+    }
+
+    /**
+     * @param emergency_contact_name the emergency_contact_name to set
+     */
+    public void setEmergency_contact_name(String emergency_contact_name) {
+        this.emergency_contact_name = emergency_contact_name;
+    }
+
+    /**
+     * @return the emergency_contact_mobile
+     */
+    public String getEmergency_contact_mobile() {
+        return emergency_contact_mobile;
+    }
+
+    /**
+     * @param emergency_contact_mobile the emergency_contact_mobile to set
+     */
+    public void setEmergency_contact_mobile(String emergency_contact_mobile) {
+        this.emergency_contact_mobile = emergency_contact_mobile;
+    }
+
+    /**
+     * @return the organisation_name
+     */
+    public String getOrganisation_name() {
+        return organisation_name;
+    }
+
+    /**
+     * @param organisation_name the organisation_name to set
+     */
+    public void setOrganisation_name(String organisation_name) {
+        this.organisation_name = organisation_name;
+    }
+
+    /**
+     * @return the org_office_name
+     */
+    public String getOrg_office_name() {
+        return org_office_name;
+    }
+
+    /**
+     * @param org_office_name the org_office_name to set
+     */
+    public void setOrg_office_name(String org_office_name) {
+        this.org_office_name = org_office_name;
+    }
+
+    /**
+     * @return the off_address_line1
+     */
+    public String getOff_address_line1() {
+        return off_address_line1;
+    }
+
+    /**
+     * @param off_address_line1 the off_address_line1 to set
+     */
+    public void setOff_address_line1(String off_address_line1) {
+        this.off_address_line1 = off_address_line1;
+    }
+
+    /**
+     * @return the off_email_id1
+     */
+    public String getOff_email_id1() {
+        return off_email_id1;
+    }
+
+    /**
+     * @param off_email_id1 the off_email_id1 to set
+     */
+    public void setOff_email_id1(String off_email_id1) {
+        this.off_email_id1 = off_email_id1;
+    }
+
+    /**
+     * @return the off_mobile_no1
+     */
+    public String getOff_mobile_no1() {
+        return off_mobile_no1;
+    }
+
+    /**
+     * @param off_mobile_no1 the off_mobile_no1 to set
+     */
+    public void setOff_mobile_no1(String off_mobile_no1) {
+        this.off_mobile_no1 = off_mobile_no1;
+    }
+
+    /**
+     * @return the designation
+     */
+    public String getDesignation() {
+        return designation;
+    }
+
+    /**
+     * @param designation the designation to set
+     */
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    private int key_person_id, emp_code, designation_code;
+    private String key_person_name, kp_address_line1, kp_address_line2, kp_address_line3, kp_mobile_no1, kp_email_id1, kp_father_name, kp_date_of_birth,
+            emergency_contact_name, emergency_contact_mobile, organisation_name, org_office_name, off_address_line1, off_email_id1,
+            off_mobile_no1, designation, organisation_code, org_office_code,org_office_type;
+
+    /**
+     * @return the organisation_code
+     */
+    public String getOrganisation_code() {
+        return organisation_code;
+    }
+
+    /**
+     * @param organisation_code the organisation_code to set
+     */
+    public void setOrganisation_code(String organisation_code) {
+        this.organisation_code = organisation_code;
+    }
+
+    /**
+     * @return the org_office_code
+     */
+    public String getOrg_office_code() {
+        return org_office_code;
+    }
+
+    /**
+     * @param org_office_code the org_office_code to set
+     */
+    public void setOrg_office_code(String org_office_code) {
+        this.org_office_code = org_office_code;
+    }
+
+    /**
+     * @return the org_office_type
+     */
+    public String getOrg_office_type() {
+        return org_office_type;
+    }
+
+    /**
+     * @param org_office_type the org_office_type to set
+     */
+    public void setOrg_office_type(String org_office_type) {
+        this.org_office_type = org_office_type;
+    }
+
+}
